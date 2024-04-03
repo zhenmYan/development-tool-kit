cc42c08884943fc103dba3cb6dd31471cf8787d0
颜真明 <zm_yan@yeah.net>
2024-04-03 21:23:37 +0800

    feat(环境搭建):提交

--- spring-boot-2.0.6.RELEASE/spring-boot-yzm/src/main/java/com/yzm/controller/HelloWorldController.java
+++ spring-boot-2.0.6.RELEASE/spring-boot-yzm/src/main/java/com/yzm/controller/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.yzm.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * description:
  *
  * @author yzm
  * @date 2024/4/3  19:25
  */
+@RestController
 public class HelloWorldController {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "hello World";
+	}
 }
