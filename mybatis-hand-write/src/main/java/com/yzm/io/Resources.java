0076716aae58e6970cd27f13c2f4cbd17f4b4809
颜真明 <zm_yan@yeah.net>
2024-04-12 18:49:10 +0800

    feat(mybatis):加载配置类

---
+++ mybatis-hand-write/src/main/java/com/yzm/io/Resources.java
@@ -0,0 +1,21 @@
+package com.yzm.io;
+
+import java.io.InputStream;
+
+/**
+ * description:
+ *
+ * @author yzm
+ * @date 2024/4/12  18:42
+ */
+public class Resources {
+
+    // 加载配置文件
+    public static InputStream getResourceAsStream(String path){
+        InputStream resourceAsStream
+                = Resources.class.getClassLoader().getResourceAsStream(path);
+        return resourceAsStream;
+    }
+
+
+}
