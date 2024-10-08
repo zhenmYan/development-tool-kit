be6a8fd3c7193a41eba38d3ba81e8a99602775fb
颜真明 <zm_yan@yeah.net>
2024-10-08 18:29:00 +0800

    feat(算法)：每日一题-1436

---
+++ algorithm/target/classes/src/main/java/com/yzm/每日一题/DailySolution1436.java
@@ -0,0 +1,30 @@
+package java.com.yzm.每日一题;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * description:
+ *
+ * @author yzm
+ * @date 2024/10/8
+ */
+public class DailySolution1436 {
+    public String destCity(List<List<String>> paths) {
+        String des = null;
+        Set<String> set = new HashSet<String>();
+        for (int i = 0; i < paths.size(); i++) {
+            List<String> path = paths.get(i);
+            set.add(path.get(0));
+        }
+        for (int i = 0; i < paths.size(); i++) {
+            List<String> path = paths.get(i);
+            if (!set.contains(path.get(1))) {
+                des = path.get(1);
+                break;
+            }
+        }
+        return des;
+    }
+}
